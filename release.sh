0c6fb5a2fba889feccadadeb7405459f3a264ff7
Andrei Finski <Andrei_Finski@epam.com>
2018-11-15 11:49:00 +0300

    Overcome Travis fork bomb

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Travis + Gradle release"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git config --global push.default current
+git stash
+git remote set-url origin https://andreifinski:$GH_TOKEN@github.com/andreifinski/agent-java-spock
+git checkout master
+git update-index --chmod=+x gradlew
+chmod +x gradlew
+./gradlew release -Prelease.useAutomaticVersion=true --debug --stacktrace -Dbintray.user=$BINTRAY_USER -Dbintray.key=$BINTRAY_KEY -Dbuild.number=$TRAVIS_BUILD_NUMBER
