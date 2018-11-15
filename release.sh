d1f959659f554d05ed105a85eb710f7fbdf53f72
Andrei Finski <Andrei_Finski@epam.com>
2018-11-15 16:36:13 +0300

    Fixed bad use of '>'

--- release.sh
+++ release.sh
@@ -5,8 +5,8 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default current
 git stash
-git remote set-url origin https://andreifinski:$GH_TOKEN@github.com/andreifinski/agent-java-spock
+git remote set-url origin https://$GH_TOKEN@github.com/andreifinski/agent-java-spock
 git checkout master
 git update-index --chmod=+x gradlew
 chmod +x gradlew
-./gradlew release bintrayUpload -Prelease.useAutomaticVersion=true --debug --stacktrace -Dbintray.user=$BINTRAY_USER -Dbintray.key=$BINTRAY_KEY -Dbuild.number=$TRAVIS_BUILD_NUMBER
+./gradlew release -Prelease.useAutomaticVersion=true --debug --stacktrace
